070b070ebdca50d4e1aa7f6d193a1a5798ee344f
frozenfung <frozenfung@gmail.com>
2014-10-24 09:52:59 +0800

    add repsond_to xml json atom

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,13 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.page(params[:page]).per(5)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @events.to_xml }
+      format.json { render :json => @events.to_json }
+      format.atom { @feed_title = "My event list" } # index.atom.builder
+    end
   end
   
 
@@ -21,13 +28,18 @@ class EventsController < ApplicationController
       flash[:notice] = "event was successfully updated"
       redirect_to events_url
     else
-      render :action => :new
+      render new_event_url
     end
   end
 
   # GET
   def show
     @page_title = @event.name
+    respond_to do |format|
+      format.html { @page_title = @event.name } # show.html.erb
+      format.xml # show.xml.builder
+      format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+    end
   end
 
   # GET
@@ -39,9 +51,9 @@ class EventsController < ApplicationController
   # GET
   def update
     if @event.update_attributes(event_params)
-      redirect_to :action => event_url(@event)
+      redirect_to event_url(@event)
     else
-      render :action => :new
+      render new_event_url
     
     end
   end
